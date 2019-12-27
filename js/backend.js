d8195567657f8d4edb2f401a8cbcb26481fa47d1
Arthur Santos Almeida <42553658+DeveloperArthur@users.noreply.github.com>
2019-12-27 14:33:45 -0300

    Add files via upload

---
+++ js/backend.js
@@ -0,0 +1,23 @@
+function erroAlert() {
+	alert('Deu erro no meu sistema ~_~ \n e me apaixonei <3');
+}
+
+function mostrarImagem(){
+	document.getElementById('amorArthur').innerHTML = "";
+	var img = document.createElement("IMG");
+	var foto = Math.floor(Math.random() * 10);
+    img.src = "imgs/"+foto+".jpg";
+    document.getElementById('amorArthur').appendChild(img);
+}
+
+function mostrarGarotaMaisLinda(){
+	document.getElementById('idGarotaMaisLinda').innerHTML = "";
+	var img = document.createElement("IMG");
+	var foto = Math.floor(Math.random() * 10);
+    img.src = "imgs/"+foto+".jpg";
+    document.getElementById('idGarotaMaisLinda').appendChild(img);
+}
+
+function mostrarHulkSantana(){
+	window.location.href = 'hulkSantana.html';
+}

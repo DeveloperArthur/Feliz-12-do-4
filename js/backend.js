a85f4d0cf914f54e535d3cdb77594dee9dcfe6ba
Arthur Santos Almeida <42553658+DeveloperArthur@users.noreply.github.com>
2020-03-23 19:55:00 -0300

    Add files via upload

--- js/backend.js
+++ js/backend.js
@@ -21,3 +21,11 @@ function mostrarGarotaMaisLinda(){
 function mostrarHulkSantana(){
     window.location.href = 'hulkSantana.html';
 }
+
+function slideFotosCasal(){
+    document.getElementById('idFotoCasal').innerHTML = "";
+    var img = document.createElement("IMG");
+    var foto = Math.floor(Math.random() * (14) + 100);
+    img.src = "imgs/"+foto+".jpg";
+    document.getElementById('idFotoCasal').appendChild(img);   
+}
